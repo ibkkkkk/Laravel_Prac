e87cc33614308b325598c2869ba118d88251b947
ibkkkkk <ruiifg40@gmail.com>
2023-08-03 15:57:04 +0900

    create categories table and models

---
+++ database/migrations/2023_08_03_152403_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('primary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('sort_order');
+            $table->timestamps();
+        });
+
+        Schema::create('secondary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('primary_category_id')->constrained();
+            $table->string('name');
+            $table->integer('sort_order');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('secondary_categories');
+        Schema::dropIfExists('primary_categories');
+    }
+};
