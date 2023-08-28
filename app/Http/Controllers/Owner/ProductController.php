e3dc91b6ac9ab9922f2e77edbc14011756492bd5
ibkkkkk <ruiifg40@gmail.com>
2023-08-28 14:04:41 +0900

    create ProductController update

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -13,6 +13,7 @@ use App\Models\PrimaryCategory;
 use App\Models\Shop;
 use App\Models\Image;
 use App\Models\Stock;
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
@@ -69,24 +70,8 @@ class ProductController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        // dd($request);
-        // $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'information' => 'required|string|max:1000',
-        //     'price' => 'required|integer',
-        //     'sort_order' => 'nullable|integer',
-        //     'quantity' => 'required|integer',
-        //     'shop_id' => 'required|exists:shop_id',
-        //     'category' => 'required|exists:secondary_categories,id',
-        //     'image1' => 'nullable|exists:images,id',
-        //     'image2' => 'nullable|exists:images,id',
-        //     'image3' => 'nullable|exists:images,id',
-        //     'image4' => 'nullable|exists:images,id',
-        //     'is_selling' => 'required'
-        // ]);
-
         try {
             DB::transaction(function () use ($request) {
                 $product = Product::create([
@@ -123,15 +108,26 @@ class ProductController extends Controller
     }
 
 
-    public function show($id)
-    {
-        //
-    }
+    // public function show($id)
+    // {
+    //     //
+    // }
 
 
     public function edit($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
+        $shops = Shop::where('owner_id', Auth::id())->select('id', 'name')->get();
+        $images = Image::where('owner_id', Auth::id())->select('id', 'filename', 'title')
+            ->orderBy('updated_at', 'desc')->get();
+        $categories = PrimaryCategory::with('secondary')->get();
+
+        return view(
+            'owner.products.edit',
+            compact('product', 'quantity', 'shops', 'images', 'categories')
+        );
     }
 
     /**
@@ -141,9 +137,66 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
-        //
+        $request->validate([
+            'current_quantity' => 'required|integer'
+        ]);
+
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
+
+        if ($request->current_quantity !== $quantity) {
+            $id = $request->route()->parameter('product');
+            return redirect()->route('owner.products.edit', ['product' => $id])
+                ->with([
+                    'message' => '在庫数が変更されています。',
+                    'status' => 'info'
+                ]);
+        } else {
+
+            try {
+                DB::transaction(function () use ($request, $product) {
+
+                    $product->name = $request->name;
+                    $product->information = $request->information;
+                    $product->price = $request->price;
+                    $product->sort_order = $request->sort_order;
+                    $product->shop_id = $request->shop_id;
+                    $product->secondary_category_id = $request->category;
+                    $product->image1 = $request->image1;
+                    $product->image2 = $request->image2;
+                    $product->image3 = $request->image3;
+                    $product->image4 = $request->image4;
+                    $product->is_selling = $request->is_selling;
+                    $product->save();
+
+                    if ($request->type === '1') {
+                        $newQuantity = $request->quantity;
+                    }
+                    if ($request->type === '2') {
+                        $newQuantity = $request->quantity * -1;
+                    }
+
+                    Stock::create([
+                        'product_id' => $product->id,
+                        'type' => $request->type,
+                        'quantity' => $newQuantity
+                    ]);
+                }, 2);
+            } catch (\Exception $e) {
+                Log::error($e);
+                throw $e;
+            }
+
+            return redirect()
+                ->route('owner.products.index')
+                ->with([
+                    'message' => '商品情報を更新しました。',
+                    'status' => 'info'
+                ]);
+        }
     }
 
     /**
---
+++ resources/views/owner/products/edit.blade.php
@@ -0,0 +1,123 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <x-flash-message status="session('status')" />
+                    <x-error :errors='$errors' />
+                    <form method="post" action="{{ route('owner.products.update', ['product' => $product->id]) }}">
+                        @csrf
+                        @method('put')
+                        <div class="-m-2">
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="name" class="leading-7 text-sm text-gray-600">商品名</label>
+                                    <input type="text" id="name" name="name" value="{{ $product->name }}"
+                                        required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="information" class="leading-7 text-sm text-gray-600">商品情報</label>
+                                    <textarea id="information" name="information" required rows="8" placeholder="{{ $product->information }}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="price" class="leading-7 text-sm text-gray-600">価格</label>
+                                    <input type="number" id="price" name="price" value="{{ $product->price }}"
+                                        required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="sort_order" class="leading-7 text-sm text-gray-600">表示順</label>
+                                    <input type="number" id="sort_order" name="sort_order"
+                                        value="{{ $product->sort_order }}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫</label>
+                                    <input type="hidden" id="current_quantity" name="current_quantity"
+                                        value="{{ $quantity }}">
+                                    <div class="w-full bg-gray-100 bg-opacity-50 rounded">{{ $quantity }}</div>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div><input type="radio" name="type" value="1" class="mr-2" checked>追加
+                                    </div>
+                                    <div><input type="radio" name="type" value="2" class="mr-2">削減</div>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="quantity" class="leading-7 text-sm text-gray-600">数量</label>
+                                    <input type="number" id="quantity" name="quantity" value="{{ old('quantity') }}"
+                                        required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <span class="text-sm">0〜99の範囲で入力してください</span>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="shop_id" class="leading-7 text-sm text-gray-600">販売する店舗</label>
+                                    <select name="shop_id" id="shop_id"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        @foreach ($shops as $shop)
+                                            <option @if ($shop->id === $product->shop_id) selected @endif
+                                                value="{{ $shop->id }}">
+                                                {{ $shop->name }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="category" class="leading-7 text-sm text-gray-600">カテゴリー</label>
+                                    <select name="category" id="category"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        @foreach ($categories as $category)
+                                            <optgroup label="{{ $category->name }}">
+                                                @foreach ($category->secondary as $secondary)
+                                                    <option @if ($secondary->id === $product->secondary_category_id) selected @endif
+                                                        value="{{ $secondary->id }}">
+                                                        {{ $secondary->name }}
+                                                    </option>
+                                                @endforeach
+                                            </optgroup>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div><input type="radio" name="is_selling" value="1" class="mr-2"
+                                            @if ($product->is_selling === 1) { checked } @endif>販売中</div>
+                                    <div><input type="radio" name="is_selling" value="0" class="mr-2"
+                                            @if ($product->is_selling === 0) { checked } @endif>停止中</div>
+                                </div>
+                            </div>
+                            <div class="p-2 w-full flex justify-center mt-4">
+                                <button type='button' onclick="location.href='{{ route('owner.products.index') }}'"
+                                    class="mx-auto bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">戻る</button>
+                                <button type="submit"
+                                    class="mx-auto bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">更新</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+</x-app-layout>
