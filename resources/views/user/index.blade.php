708e2f59b800001ad325826b1361109b3c5ad4e1
ibkkkkk <ruiifg40@gmail.com>
2023-08-30 13:39:29 +0900

    create factroy using faker

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,1 +1,29 @@
-sample;
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('ホーム') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="flex flex-wrap w-full">
+                        @foreach ($products as $product)
+                            <div class="w-1/4 h-1/2">
+                                <a class="" href=""></a>
+                                <div class="object-cover">
+                                    <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}"
+                                        type='products' />
+                                </div>
+                                </a>
+                            </div>
+                        @endforeach
+                        <p>sample</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
